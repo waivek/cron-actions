37b404ae9fb614ba81f0460fee220d88964eb2c1
Vivek Bose <waivek@gmail.com>
2022-01-02 22:28:57 +0530

    Commit.

--- print_date.py
+++ print_date.py
@@ -1,7 +1,13 @@
+# import sys
+# for path in sys.path:
+#     print(path)
 
-from datetime import datetime
-import pytz
-#
 
-dt = datetime.now(pytz.timezone('Asia/Kolkata'))
-print(dt.strftime("%H:%M %p"))
+# from datetime import datetime
+# import pytz
+# dt = datetime.now(pytz.timezone('Asia/Kolkata'))
+# print(dt.strftime("%H:%M %p"))
+
+from common import Date
+dt = Date.now()
+print(repr(dt))
