a8f3458a61a5da1d4a32480ddc931c01a5ca472c
Vivek Bose <waivek@gmail.com>
2022-01-04 18:17:09 +0530

    Commit.

--- test_upsert.py
+++ test_upsert.py
@@ -1,20 +1,12 @@
 
 def sqlite3_test():
-    import sys
-    if sys.platform == "linux":
-        import pysqlite3 as sqlite3
-    else:
-        import sqlite3
-
+    from db import db_init, sqlite3
     pysqlite_version = sqlite3.version
     sqlite_version = sqlite3.sqlite_version
     print(f"PYSQLITE: {pysqlite_version}")
     print(f"SQLITE  : {sqlite_version}")
 
-    db_path = "sqlite_test.db"
-    connection = sqlite3.connect(db_path)
-    connection.row_factory = sqlite3.Row
-    cursor = connection.cursor()
+    cursor, connection = db_init("sqlite_test.db")
     cursor.execute("DROP TABLE IF EXISTS nba_players;")
     cursor.execute("CREATE TABLE nba_players (name TEXT PRIMARY KEY, team TEXT);")
     cursor.execute("INSERT INTO nba_players (name, team) VALUES (?, ?);", ("LeBron James", "Cleaveland Cavaliers"))
